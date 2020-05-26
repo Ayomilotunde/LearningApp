4fb2c04a193a162394e7975c3559ace2b55bb1e8
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-05-26 19:33:02 +0100

    update

--- app/src/main/java/com/ayomi/newapp/MainActivity.java
+++ app/src/main/java/com/ayomi/newapp/MainActivity.java
@@ -1,14 +1,88 @@
 package com.ayomi.newapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "message";
+    private FirebaseAuth mAuth;
+    EditText mEmail, mPassword;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        signUp = findViewById(R.id.signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
+
+
+
+    private void updateUI(FirebaseUser user) {
+    }
+
+    public void signUpButton(View view) {
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(MainActivity.this, SuccessfulLoginActivity.class);
+                            startActivity(intent);
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+
+
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }*/
 }
